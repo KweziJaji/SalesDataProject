0656fc49aa12097aeae4a4cc9827d4df6b034c70
Kwezilomso Jaji <48382234+KweziJaji@users.noreply.github.com>
2024-04-16 01:00:01 +0200

    Create sales_gcs_partitioned.py

---
+++ sales_project/data_exporters/sales_gcs_partitioned.py
@@ -0,0 +1,30 @@
+import pyarrow as pa 
+import pyarrow.parquet as pq 
+import os
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/src/turkish_retail_sales/gc-creds.json'
+
+project_id = 'salesdataproject-kj'
+
+table_name = 'turkish_sales_partitioned'
+
+bucket_name = 'sales-bucket-kj'
+
+root_path = f"{bucket_name}/{table_name}"
+
+@data_exporter
+def export_data(data, *args, **kwargs):
+
+    table = pa.Table.from_pandas(data)
+
+    gcs = pa.fs.GcsFileSystem()
+
+    pq.write_to_dataset(
+        table,
+        root_path = root_path,
+        partition_cols = ['date'],
+        filesystem = gcs
+    )
